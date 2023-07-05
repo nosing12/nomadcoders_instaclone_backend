d137950a864dca33e69bc1a2a887a9718b5bbf72
rooney <bch3454@gmail.com>
2023-07-05 09:45:08 +0900

    Feat: Add create account mutation
    - bcrypt : using hashing for password , https://www.npmjs.com/package/bcrypt

---
+++ users/users.mutations.js
@@ -0,0 +1,40 @@
+import bcrypt from "bcrypt";
+import client from "../client.js";
+
+export default {
+  Mutation: {
+    createAccount: async (
+      _,
+      { firstName, lastName, username, email, password }
+    ) => {
+      // check if username or email are already on DB.
+      const existingUser = await client.user.findFirst({
+        where: {
+          OR: [
+            {
+              username,
+            },
+            {
+              email,
+            },
+          ],
+        },
+      });
+      console.log(existingUser);
+
+      // hash password
+      const uglyPassword = await bcrypt.hash(password, 10);
+
+      // save and return the user
+      return client.user.create({
+        data: {
+          username,
+          email,
+          firstName,
+          lastName,
+          password: uglyPassword,
+        },
+      });
+    },
+  },
+};
---
+++ users/users.typeDefs.js
@@ -0,0 +1,25 @@
+const typeDefs = `#graphql
+  type User {
+    id: String!
+    firstName: String!
+    lastName: String
+    username: String!
+    email: String!
+    createdAt: String!
+    updatedAt: String!
+  }
+  type Mutation {
+    createAccount(
+      firstName: String!
+      lastName: String
+      username: String!
+      email: String!
+      password: String!
+    ): User
+  }
+  type Query {
+    seeProfile(username: String!): User
+  }
+`;
+
+export default typeDefs;
